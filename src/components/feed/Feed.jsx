c458b2920e9acf32727a85238ecdc3c8460f81a6
Ankush <ankush@opencubicles.com>
2021-06-19 18:33:49 +0530

    changes

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,10 +1,17 @@
-import './feed.css'
+import Post from "../post/Post";
+import Share from "../share/Share";
+import "./feed.css";
+import { Posts } from "../../dummyData";
 
-export default function Feed(){
-  return(
+export default function Feed() {
+  return (
     <div className="feed">
-    Feed
+      <div className="feedWrapper">
+        <Share />
+        {Posts.map((p) => (
+          <Post key={p.id} post={p} />
+        ))}
+      </div>
     </div>
-  )
-
+  );
 }
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,38 +1,42 @@
-import './topbar.css'
-import {Search , Person, Notifications} from '@material-ui/icons';
+import "./topbar.css";
+import { Search, Person, Chat, Notifications } from "@material-ui/icons";
 
-export default function Topbar(){
-  return(
+export default function Topbar() {
+  return (
     <div className="topbarContainer">
       <div className="topbarLeft">
-        <span className="logo">Technoera</span>
+        <span className="logo">Bookcafe</span>
       </div>
       <div className="topbarCenter">
         <div className="searchbar">
           <Search className="searchIcon" />
-          <input placeholder="Search for Questions , friends" className="searchInput" />
+          <input
+            placeholder="Search for friend, post or video"
+            className="searchInput"
+          />
         </div>
       </div>
       <div className="topbarRight">
         <div className="topbarLinks">
-          <span className="topbarLink" >Statistics</span>
-          <span className="topbarLink" >Questions</span>
-          <span className="topbarLink" >Profile</span>
+          <span className="topbarLink">Homepage</span>
+          <span className="topbarLink">Timeline</span>
         </div>
         <div className="topbarIcons">
-          <div className="topbarIconItem" >
+          <div className="topbarIconItem">
             <Person />
             <span className="topbarIconBadge">1</span>
           </div>
-          <div className="topbarIconItem" >
+          <div className="topbarIconItem">
+            <Chat />
+            <span className="topbarIconBadge">2</span>
+          </div>
+          <div className="topbarIconItem">
             <Notifications />
             <span className="topbarIconBadge">1</span>
           </div>
         </div>
-        <img src="/assets/persons/5.jpeg" alt="" className="topbarImg" />
+        <img src="/assets/person/1.jpeg" alt="" className="topbarImg"/>
       </div>
-
     </div>
-  )
-
+  );
 }
